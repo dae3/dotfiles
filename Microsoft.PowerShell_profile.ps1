fb37b74afde4f40f062dfabd6e0890d0be7f3be1
Daniel Everett <deverett@gmail.com>
2020-02-14 19:03:52 +1100

    Add Connect-CdhVM function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,4 +6,35 @@ function Stop-NamedProcess {
     Get-Process | Where-Object Name -Like "*$processName*" | Stop-Process -Force
 }
 
-# just a test
+function Connect-CdhVm {
+    Param(
+	    [parameter(Mandatory=$true)] [ValidateSet("dev","sit","uat","prod")][String] $Environment,
+	    [parameter(Mandatory=$true)] [ValidateSet("mule","onto","mtls")] [String] $HostName
+    )
+
+    $baseHost = switch($hostName) {
+	    "mule" { "muleworker" }
+	    "onto" { "ontoserver" }
+	    "mtls" { "mtls" }
+	}
+    $userName = switch($hostName) {
+	    "mule" { "muleuser" }
+	    "onto" { "ontouser" }
+	    "mtls" { "mtlsuser" }
+	}
+
+    $keyPath = "${ENV:APPDATA}\cdhkeys\${environment}-${hostName}.key"
+    if (-Not (Test-Path $keyPath)) {
+	Write-Host -ForegroundColor Yellow "Fetching private key from Key Vault..."
+	    if (-Not (Test-Path "${ENV:APPDATA}\cdhkeys")) {
+		Write-Host -ForegroundColor Yellow "Creating key cache..."
+		New-Item -Path "${ENV:APPDATA}\cdhkeys" -ItemType Directory | Out-Null
+	    }
+	az keyvault secret download --vault-name "ncdhc-${environment}-1" --name "ncdhc-${environment}-${baseHost}-key" --file $keyPath
+    } else {
+	Write-Host -ForegroundColor Yellow "Using cached key $keyPath"
+    }
+    $fullhostname = "${userName}@ncdhc-${environment}-${baseHost}.australiaeast.cloudapp.azure.com"
+    Write-Host -ForegroundColor Green "Connecting to $fullhostname..."
+    Start-Process -FilePath ssh.exe -ArgumentList ("-i", $keyPath, $fullhostname) -NoNewWindow -Wait
+}
