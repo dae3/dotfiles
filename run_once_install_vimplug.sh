500158e36782ed22072849dd1ae604dd22bbdb76
Daniel Everett <deverett@gmail.com>
2024-01-21 14:50:09 +1100

    Add vim-plug bootstrap script

---
+++ run_once_install_vimplug.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Looks like a new vim install, downloading vim-plug..."
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ $? -eq 0 ]; then
+  echo "Done"
+else
+  echo "Failed"
+fi
